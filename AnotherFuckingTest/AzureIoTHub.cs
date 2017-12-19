73450ddc2a5a6ca23e6f0849b2a68cb4cdc66292
calvin bootsman <calvinbootsman@hotmail.com>
2017-12-19 23:27:31 +0100

    Add project files.

---
+++ AnotherFuckingTest/AzureIoTHub.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+
+class AzureIoTHub
+{
+    private static void CreateClient()
+    {
+        if (deviceClient == null)
+        {
+            // create Azure IoT Hub client from embedded connection string
+            deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+        }
+    }
+
+    static DeviceClient deviceClient = null;
+
+    //
+    // Note: this connection string is specific to the device "test". To configure other devices,
+    // see information on iothub-explorer at http://aka.ms/iothubgetstartedVSCS
+    //
+    const string deviceConnectionString = "HostName=EPDEgroup8Hanze.azure-devices.net;DeviceId=test;SharedAccessKey=KZrtUfQVHwvKY/tcreHOAnBStxLwVI7Bl+9NY+zmVnk=";
+
+
+    //
+    // To monitor messages sent to device "kraaa" use iothub-explorer as follows:
+    //    iothub-explorer monitor-events --login HostName=EPDEgroup8Hanze.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=MDNZ9Uwt0UoLUn+UXWrDuGXhf4JT4xVxHygRpzdhprU= "test"
+    //
+
+    // Refer to http://aka.ms/azure-iot-hub-vs-cs-2017-wiki for more information on Connected Service for Azure IoT Hub
+
+    public static async Task SendDeviceToCloudMessageAsync()
+    {
+        CreateClient();
+#if WINDOWS_UWP
+        var str = "{\"deviceId\":\"test\",\"messageId\":1,\"text\":\"Hello, Cloud from a UWP C# app!\"}";
+#else
+        var str = "{\"deviceId\":\"test\",\"messageId\":1,\"text\":\"Hello, Cloud from a C# app!\"}";
+#endif
+        var message = new Message(Encoding.ASCII.GetBytes(str));
+
+        await deviceClient.SendEventAsync(message);
+    }
+
+    public static async Task<string> ReceiveCloudToDeviceMessageAsync()
+    {
+        CreateClient();
+
+        while (true)
+        {
+            var receivedMessage = await deviceClient.ReceiveAsync();
+
+            if (receivedMessage != null)
+            {
+                var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                await deviceClient.CompleteAsync(receivedMessage);
+                return messageData;
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
---
+++ AnotherFuckingTest/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AnotherFuckingTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var message = await AzureIoTHub.ReceiveCloudToDeviceMessageAsync();
+                    Debug.WriteLine(message);
+                    await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => { textBlock.Text += message; });
+                }
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Task.Run(async () => { await AzureIoTHub.SendDeviceToCloudMessageAsync(); });
+            }
+        }
+    }
+}
